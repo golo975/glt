48f925655e82b14a365fb09eafdf65c3073e760b
gaolong <golomail975@gmail.com>
2014-10-25 23:54:38 +0800

    终于把spring mvc的流程跑通了。

--- sourceCode/test/src/com/thunisoft/glt/spring/mvc/controller/LoginController.java
+++ sourceCode/test/src/com/thunisoft/glt/spring/mvc/controller/LoginController.java
@@ -1,28 +1,40 @@
 package com.thunisoft.glt.spring.mvc.controller;
 
+import java.io.IOException;
+import java.io.Writer;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
-	/**
-	 * 
-	 * @return
-	 */
-	@RequestMapping(value = "/login/{user}", method = RequestMethod.GET)
 	/*
 	 * @PathVariable 注解可以自动将URI中的参数注入到方法的参数中 这个URI可以是方法上的@PathVariable
 	 * ，也可以是类上的指定的 这里也支持正则表达式，例如{version:\\d\\.\\d\\.\\d}{extension:\\.[a-z]}，
 	 * 其中version和extension是参数的名字，用于在其他地方引用
 	 * URI中的${...}中的占位符还可以直接获取属性文件中的值，所以可以把controller的path配置到属性文件中
 	 * 。详见：PropertyPlaceholderConfigurer 这里还可以使用Matrix variables，现在还没弄明白，以后再看。
+	 * 
+	 * @RequestParam用来获取请求中的参数。
 	 */
-	public String login(@PathVariable("user") String userName) {
+	@RequestMapping(value = "/login/{user}", method = RequestMethod.GET)
+	public String login(@PathVariable("user") String userName,
+			@RequestParam("gender") String gender) {
 		System.out.println(userName);
-		System.out.println(1234);
-		return null;
+		System.out.println(gender);
+		return "target";
 	}
+
+	@RequestMapping(value = "/something", method = RequestMethod.GET)
+	public void handle(@RequestBody String body, Writer writer)
+			throws IOException {
+		System.out.println(body);
+//		writer.write(body);
+	}
+
 }
