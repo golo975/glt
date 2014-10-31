3afb6115ea1abf4f5f036bb01a31c0ec381b1802
gaolong <gaolong@gaolongTP>
2014-10-31 18:59:17 +0800

    添加比较两个文件的工具类。 修改Dao层使用JdbcDaoSupport。

--- sourceCode/test/src/com/thunisoft/glt/persistence/UserDao.java
+++ sourceCode/test/src/com/thunisoft/glt/persistence/UserDao.java
@@ -7,10 +7,12 @@ import java.sql.Statement;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
 import org.springframework.stereotype.Repository;
 
 @Repository("userDao")
-public class UserDao {
+public class UserDao extends JdbcDaoSupport{
 
 	private static final Log logger = LogFactory.getLog(UserDao.class);
 	public int jdbcInsert(String username) {
@@ -22,10 +24,15 @@ public class UserDao {
 			String sql = "INSERT INTO gltuser(username)  VALUES ('" + username + "')";
 			st.execute(sql);
 		} catch (ClassNotFoundException e) {
-			logger.error("", e);
+			logger.error(e.getMessage(), e);
 		} catch (SQLException e) {
-			logger.error("", e);
+			logger.error(e.getMessage(), e);
 		}
 		return 0;
 	}
+	
+//	public int jdbcTemplateInsert(String username){
+//		JdbcTemplate jdbcTemplate = new JdbcTemplate();
+//		jdbcTemplate.update(sql);
+//	}
 }
