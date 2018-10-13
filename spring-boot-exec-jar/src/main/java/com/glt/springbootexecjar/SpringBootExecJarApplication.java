dc43346ae42f38932538781c6f6086c2af6a781a
gaolong <golomail975@gmail.com>
2018-10-13 20:43:21 +0800

    spring-boot-exec-jar init

---
+++ spring-boot-exec-jar/src/main/java/com/glt/springbootexecjar/SpringBootExecJarApplication.java
@@ -0,0 +1,12 @@
+package com.glt.springbootexecjar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExecJarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootExecJarApplication.class, args);
+    }
+}
