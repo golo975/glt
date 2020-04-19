669ce35ddc00b38d850b3e3e0bb6e4248c2c4736
gaolong <golomail975@gmail.com>
2020-04-19 21:07:51 +0800

    。。。

--- spring-boot-demo-1/src/main/java/com/gaolong/springbootdemo1/StampedLockDemo.java
+++ spring-boot-demo-1/src/main/java/com/gaolong/springbootdemo1/StampedLockDemo.java
@@ -32,16 +32,8 @@ public class StampedLockDemo {
             return (int) Math.sqrt(_x * _x + _y * _y);
         }
 
-        public int getX() {
-            return x;
-        }
-
         public void setX(int x) {
             this.x = x;
-        }
-
-        public int getY() {
-            return y;
         }
 
         public void setY(int y) {
