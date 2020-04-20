02aecabdcfb0ab8fa859d86178c3ea834288314c
gaolong <golomail975@gmail.com>
2020-04-20 18:20:06 +0800

    ...

--- tag-everything/src/main/java/com/gaolong/tageverything/nio/netty/TimeServer.java
+++ tag-everything/src/main/java/com/gaolong/tageverything/nio/netty/TimeServer.java
@@ -17,13 +17,14 @@ public class TimeServer {
         EventLoopGroup workerGroup = new NioEventLoopGroup();
 
         try {
-            ServerBootstrap b = new ServerBootstrap();
-            b.group(bossGroup, workerGroup)
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+
+            serverBootstrap.group(bossGroup, workerGroup)
                     .channel(NioServerSocketChannel.class)
                     .option(ChannelOption.SO_BACKLOG, 1024)
                     .childHandler(new ChildChannelHandler());
             // 绑定端口，同步等待成功
-            ChannelFuture f = b.bind(port).sync();
+            ChannelFuture f = serverBootstrap.bind(port).sync();
 
             // 等待服务端监听端口关闭
             f.channel().closeFuture().sync();
@@ -52,7 +53,7 @@ java.base/jdk.internal.misc=ALL-UNNAMED
         int port = 8080;
         if (args != null && args.length > 0) {
             try {
-                port = Integer.valueOf(args[0]);
+                port = Integer.parseInt(args[0]);
             } catch (NumberFormatException e) {
                 e.printStackTrace();
             }
