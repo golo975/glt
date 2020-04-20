767286b9ff141fbb180fe010894d1b6fe0c11171
gaolong <golomail975@gmail.com>
2020-04-20 18:57:57 +0800

    ...

---
+++ spring-boot-dubbo-demo-1/src/main/java/com/gaolong/springbootdubbodemo1/SpringBootDubboDemo1Application.java
@@ -0,0 +1,13 @@
+package com.gaolong.springbootdubbodemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDubboDemo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDubboDemo1Application.class, args);
+    }
+
+}
