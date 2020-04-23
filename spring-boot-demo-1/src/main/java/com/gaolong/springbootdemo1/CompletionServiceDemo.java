2c746e44af004f272c4f6389f140933d1a7bdf18
gaolong <golomail975@gmail.com>
2020-04-23 23:32:58 +0800

    CompletionServiceDemo

---
+++ spring-boot-demo-1/src/main/java/com/gaolong/springbootdemo1/CompletionServiceDemo.java
@@ -0,0 +1,44 @@
+package com.gaolong.springbootdemo1;
+
+import com.gaolong.springbootdemo1.util.ExecutorUtils;
+
+import java.util.concurrent.*;
+
+public class CompletionServiceDemo {
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+
+        ExecutorService executor = Executors.newCachedThreadPool();
+        CompletionService<String> completionService = new ExecutorCompletionService<>(executor, new LinkedBlockingQueue<>(10));
+        completionService.submit(() -> {
+            ExecutorUtils.sleep(1, 1000);
+            return "1: Hello World";
+        });
+        completionService.submit(() -> {
+            ExecutorUtils.sleep(1, 1000);
+            return "2: Hello World";
+        });
+        completionService.submit(() -> {
+            ExecutorUtils.sleep(1, 1000);
+            return "3: Hello World";
+        });
+        completionService.submit(() -> {
+            ExecutorUtils.sleep(1, 1000);
+            return "4: Hello World";
+        });
+
+        // take() 方法：如果获取不到，则阻塞
+        // poll() 方法：如果获取不到，则返回null，不会阻塞
+        for (int i = 0; i < 2; i++) {
+            System.out.println(completionService.take().get());
+            Future<String> pollFuture = completionService.poll();
+            System.out.println(pollFuture);
+        }
+
+        ExecutorUtils.shutdown(executor);
+    }
+
+
+
+
+}
--- spring-boot-demo-1/src/main/java/com/gaolong/springbootdemo1/util/ExecutorUtils.java
+++ spring-boot-demo-1/src/main/java/com/gaolong/springbootdemo1/util/ExecutorUtils.java
@@ -1,9 +1,12 @@
 package com.gaolong.springbootdemo1.util;
 
+import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class ExecutorUtils {
+
+    private static Random random = new Random(System.currentTimeMillis());
 
     public static void shutdown(ExecutorService executorService) {
         executorService.shutdown();
@@ -17,6 +20,15 @@ public class ExecutorUtils {
         } catch (InterruptedException e) {
             executorService.shutdownNow();
             Thread.currentThread().interrupt();
+        }
+    }
+
+    public static void sleep(long timeout, int randomMills) {
+        try {
+            TimeUnit.SECONDS.sleep(timeout);
+            TimeUnit.MILLISECONDS.sleep(random.nextInt(randomMills));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
