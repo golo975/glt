10d0f1aac1ad98282002f2de404e665840e9b681
gaolong <golomail975@gmail.com>
2020-06-12 14:18:28 +0800

    Alibaba rocket mq demo

---
+++ spring-cloud-alibaba-rockekmq-demo-1/src/main/java/com/gaolong/rocketmq/demo1/SpringCloudAlibabaRockekmqDemo1Application.java
@@ -0,0 +1,13 @@
+package com.gaolong.rocketmq.demo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudAlibabaRockekmqDemo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudAlibabaRockekmqDemo1Application.class, args);
+    }
+
+}
