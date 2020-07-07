565f6597a1de2611d0af53ee57adb08e823fc0b1
gaolong <golomail975@gmail.com>
2020-07-07 23:17:02 +0800

    添加数据库连接池：Druid

---
+++ spring-boot-demo-1/src/test/java/com/gaolong/springbootdemo1/DruidTest.java
@@ -0,0 +1,7 @@
+package com.gaolong.springbootdemo1;
+
+/**
+ * Druid 是阿里巴巴开发的数据库连接池，它提供了强大的监控和扩展功能。
+ */
+public class DruidTest {
+}
---
+++ spring-boot-demo-1/src/test/java/com/gaolong/springbootdemo1/JsonTest.java
@@ -0,0 +1,13 @@
+package com.gaolong.springbootdemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class JsonTest {
+
+    @Test
+    public void test() {
+        
+    }
+}
